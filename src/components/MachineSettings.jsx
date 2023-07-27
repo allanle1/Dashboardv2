8c59eed8e3f3f4fa3c0374cddd54e444d5ecf60f
DileepAmrani <amranidileep44@gmail.com>
2023-07-28 04:14:04 +0500

    machine card

--- src/components/MachineSettings.jsx
+++ src/components/MachineSettings.jsx
@@ -1,24 +1,31 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import { IconButton, useTheme, TextField, Card, CardContent } from '@mui/material';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import {
+  IconButton,
+  useTheme,
+  TextField,
+  Card,
+  CardContent,
+} from "@mui/material";
 import { SettingsOutlined } from "@mui/icons-material";
 import FlexBetween from "components/FlexBetween";
 import CancelIcon from "@mui/icons-material/Cancel";
 
-function MachineSettings() {
+function MachineSettings({data, onSubmit}) {
   const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState(data?.nextservicehours);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const theme = useTheme();
 
   const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
     width: 400,
     bgcolor: theme.palette.background.alt,
     boxShadow: 24,
@@ -26,52 +33,59 @@ function MachineSettings() {
     p: 4,
   };
 
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onSubmit(value, data?.id);
+    setOpen(false)
+  };
   return (
     <div>
       <IconButton>
         <SettingsOutlined
-            onClick={handleOpen}
-            sx={{color: theme.palette.secondary[300], fontSize:"25px"}}
+          onClick={handleOpen}
+          sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
         />
-    </IconButton>  
+      </IconButton>
 
       <Modal
         open={open}
         // onClose={handleClose}
       >
         <Box sx={style}>
-        <FlexBetween>
-        <Typography
-          sx={{ fontSize: 20 }}
-          color={theme.palette.secondary[400]}
-        >
-          machineid
-        </Typography>
-        <IconButton>
-        <CancelIcon
-        onClick={handleClose}
-            sx={{color: theme.palette.secondary[300], fontSize:"25px"}}
-        />
-        </IconButton>  
-        </FlexBetween>
-        <form>
+          <FlexBetween>
+            <Typography
+              sx={{ fontSize: 20 }}
+              color={theme.palette.secondary[400]}
+            >
+              machineid: {data?.machineid}
+            </Typography>
+            <IconButton>
+              <CancelIcon
+                onClick={handleClose}
+                sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+              />
+            </IconButton>
+          </FlexBetween>
+          <form>
             <FlexBetween>
-            <TextField
-          style={{ width: "200px", margin: "5px" }}
-          type="text"
-          label="Next Service Hours"
-          variant="outlined"
-        />
-        <Button variant="contained" color="primary">
-          save
-        </Button>
+              <TextField
+                style={{ width: "200px", margin: "5px" }}
+                type="text"
+                label="Next Service Hours"
+                variant="outlined"
+                value={value}
+                onChange={(e)=> setValue(e.target.value)}
+              />
+              <Button variant="contained" color="primary" onClick={handleSubmit}>
+                save
+              </Button>
             </FlexBetween>
-      </form>
+          </form>
         </Box>
       </Modal>
     </div>
   );
 }
 
-  
 export default MachineSettings;
--- src/state/AuthContext.js
+++ src/state/AuthContext.js
@@ -1,13 +1,19 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from "react";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
   sendPasswordResetEmail,
-} from 'firebase/auth';
-import { auth } from 'firebase-config';
-import { collection, doc, getFirestore, getDoc } from 'firebase/firestore';
+} from "firebase/auth";
+import { auth } from "firebase-config";
+import {
+  collection,
+  doc,
+  getFirestore,
+  getDoc,
+  usersCollectionRef,
+} from "firebase/firestore";
 
 const UserContext = createContext();
 
@@ -18,34 +24,45 @@ export const AuthContextProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-   const signIn = (email, password) =>  {
-    return signInWithEmailAndPassword(auth, email, password)
-   }
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
   const logout = () => {
-      return signOut(auth)
-  }
+    return signOut(auth);
+  };
 
-  const resetPassword = (email ) =>  {
-    return sendPasswordResetEmail(auth, email)
-   }
-  
+  const resetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
 
   useEffect(() => {
     const firestore = getFirestore();
-    const unsubscribe = onAuthStateChanged(auth, async(loggedInUser) => {
-        if (loggedInUser) {
-            // User is logged in, get their info based on their ID
-            const userRef = doc(collection(firestore, 'users'), loggedInUser.uid);
-            const userDoc = await getDoc(userRef);
-            if (userDoc.exists()) {
-                setUser(userDoc.data());
-                console.log(userDoc.data())
-            }
+    const unsubscribe = onAuthStateChanged(auth, async (loggedInUser) => {
+      if (loggedInUser) {
+        // User is logged in, get their info based on their ID
+        const userRef = doc(collection(firestore, "users"), loggedInUser.uid);
+        const userDocSnapshot = await getDoc(userRef);
+        if (userDocSnapshot.exists()) {
+          // Get the user data from the document
+          let userData = userDocSnapshot.data();
+          const siteRef = userData.site;
+          const siteSnapshot = await getDoc(siteRef);
+          if (siteSnapshot.exists()) {
+            const siteData = siteSnapshot.data();
+            console.log(siteSnapshot);
+            const newData = { ...siteData, _uid: siteSnapshot.id };
+            userData.site = newData; // Replace the reference field with the site data
+          }
+          const newUser = { ...userData, _uid: loggedInUser.uid };
+          setUser(newUser);
         } else {
-            // User is logged out
-            setUser(null);
+          console.log("User data not found.");
         }
+      } else {
+        // User is logged out
+        setUser(null);
+      }
     });
 
     return () => {
@@ -54,7 +71,9 @@ export const AuthContextProvider = ({ children }) => {
   }, []);
 
   return (
-    <UserContext.Provider value={{ createUser, user, logout, signIn, resetPassword }}>
+    <UserContext.Provider
+      value={{ createUser, user, logout, signIn, resetPassword }}
+    >
       {children}
     </UserContext.Provider>
   );
